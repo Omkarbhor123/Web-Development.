a9872244962cc1ca127e3e573cd793b7ebf9287c
Omkar Bhor <118344494+Omkarbhor123@users.noreply.github.com>
2023-10-05 00:22:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const textarea = document.querySelector("textarea");
+const button = document.querySelector("button");
+let isSpeaking = true;
+
+const textToSpeech = () => {
+  const synth = window.speechSynthesis;
+  const text = textarea.value;
+
+  if (!synth.speaking && text) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    
+    // Get voices
+    const voices = synth.getVoices();
+
+    // Find Indian female voice by name
+    const indianFemaleVoice = voices.find(voice => voice.name === 'Google हिन्दी');
+
+    // If Indian female voice is available, use it
+    if (indianFemaleVoice) {
+      utterance.voice = indianFemaleVoice;
+    } else {
+      console.log('Indian female voice not found. Using default voice.');
+    }
+
+    synth.speak(utterance);
+  }
+
+  // Rest of your code remains unchanged
+};
+
+// Ensure the voices are loaded before using them
+window.speechSynthesis.onvoiceschanged = () => {
+  button.addEventListener("click", textToSpeech);
+};
